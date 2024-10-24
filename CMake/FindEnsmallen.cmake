931b8ef8f642d796f82f461f3f57b4458171c471
bpapaspyros <b.papaspyros@gmail.com>
2024-10-24 16:45:48 +0200

    Revert "fix: replace all src/bin dirs with current src/bin dirs"
    This reverts commit 6851a64a8790026fdddc147056d54e2bc75bbbab.

--- CMake/FindEnsmallen.cmake
+++ CMake/FindEnsmallen.cmake
@@ -2,50 +2,53 @@
 # Find the Ensmallen C++ library
 #
 # This module sets the following variables:
-# ENSMALLEN_FOUND - set to true if the library is found
-# ENSMALLEN_INCLUDE_DIR - list of required include directories
-# ENSMALLEN_VERSION_MAJOR - major version number
-# ENSMALLEN_VERSION_MINOR - minor version number
-# ENSMALLEN_VERSION_PATCH - patch version number
-# ENSMALLEN_VERSION_STRING - version number as a string (ex: "1.0.4")
-# ENSMALLEN_VERSION_NAME - name of the version (ex: "Antipodean Antileech")
+#  ENSMALLEN_FOUND - set to true if the library is found
+#  ENSMALLEN_INCLUDE_DIR - list of required include directories
+#  ENSMALLEN_VERSION_MAJOR - major version number
+#  ENSMALLEN_VERSION_MINOR - minor version number
+#  ENSMALLEN_VERSION_PATCH - patch version number
+#  ENSMALLEN_VERSION_STRING - version number as a string (ex: "1.0.4")
+#  ENSMALLEN_VERSION_NAME - name of the version (ex: "Antipodean Antileech")
 
 file(GLOB ENSMALLEN_SEARCH_PATHS
-    ${CMAKE_CURRENT_BINARY_DIR}/deps/ensmallen-[0-9]*.[0-9]*.[0-9]*)
+    ${CMAKE_BINARY_DIR}/deps/ensmallen-[0-9]*.[0-9]*.[0-9]*)
 find_path(ENSMALLEN_INCLUDE_DIR
-    NAMES ensmallen.hpp
-    PATHS ${ENSMALLEN_SEARCH_PATHS}/include)
+  NAMES ensmallen.hpp
+  PATHS ${ENSMALLEN_SEARCH_PATHS}/include)
 
 if(ENSMALLEN_INCLUDE_DIR)
-    # ------------------------------------------------------------------------
-    # Extract version information from <ensmallen>
-    # ------------------------------------------------------------------------
-    set(ENSMALLEN_VERSION_MAJOR 0)
-    set(ENSMALLEN_VERSION_MINOR 0)
-    set(ENSMALLEN_VERSION_PATCH 0)
-    set(ENSMALLEN_VERSION_NAME "unknown")
+  # ------------------------------------------------------------------------
+  #  Extract version information from <ensmallen>
+  # ------------------------------------------------------------------------
 
-    if(EXISTS "${ENSMALLEN_INCLUDE_DIR}/ensmallen_bits/ens_version.hpp")
-        set(ENSMALLEN_FOUND YES)
+  set(ENSMALLEN_VERSION_MAJOR 0)
+  set(ENSMALLEN_VERSION_MINOR 0)
+  set(ENSMALLEN_VERSION_PATCH 0)
+  set(ENSMALLEN_VERSION_NAME "unknown")
 
-        # Read and parse armdillo version header file for version number
-        file(READ "${ENSMALLEN_INCLUDE_DIR}/ensmallen_bits/ens_version.hpp"
-            _ensmallen_HEADER_CONTENTS)
-        string(REGEX REPLACE ".*#define ENS_VERSION_MAJOR ([0-9]+).*" "\\1"
-            ENSMALLEN_VERSION_MAJOR "${_ensmallen_HEADER_CONTENTS}")
-        string(REGEX REPLACE ".*#define ENS_VERSION_MINOR ([0-9]+).*" "\\1"
-            ENSMALLEN_VERSION_MINOR "${_ensmallen_HEADER_CONTENTS}")
-        string(REGEX REPLACE ".*#define ENS_VERSION_PATCH ([0-9]+).*" "\\1"
-            ENSMALLEN_VERSION_PATCH "${_ensmallen_HEADER_CONTENTS}")
+  if(EXISTS "${ENSMALLEN_INCLUDE_DIR}/ensmallen_bits/ens_version.hpp")
 
-        # WARNING: The number of spaces before the version name is not one.
-        string(REGEX REPLACE
-            ".*#define ENS_VERSION_NAME\ +\"([0-9a-zA-Z\ _-]+)\".*" "\\1"
-            ENSMALLEN_VERSION_NAME "${_ensmallen_HEADER_CONTENTS}")
-    endif()
+    set(ENSMALLEN_FOUND YES)
 
-    set(ENSMALLEN_VERSION_STRING "${ENSMALLEN_VERSION_MAJOR}.${ENSMALLEN_VERSION_MINOR}.${ENSMALLEN_VERSION_PATCH}")
-endif()
+    # Read and parse armdillo version header file for version number
+    file(READ "${ENSMALLEN_INCLUDE_DIR}/ensmallen_bits/ens_version.hpp"
+        _ensmallen_HEADER_CONTENTS)
+    string(REGEX REPLACE ".*#define ENS_VERSION_MAJOR ([0-9]+).*" "\\1"
+        ENSMALLEN_VERSION_MAJOR "${_ensmallen_HEADER_CONTENTS}")
+    string(REGEX REPLACE ".*#define ENS_VERSION_MINOR ([0-9]+).*" "\\1"
+        ENSMALLEN_VERSION_MINOR "${_ensmallen_HEADER_CONTENTS}")
+    string(REGEX REPLACE ".*#define ENS_VERSION_PATCH ([0-9]+).*" "\\1"
+        ENSMALLEN_VERSION_PATCH "${_ensmallen_HEADER_CONTENTS}")
+
+    # WARNING: The number of spaces before the version name is not one.
+    string(REGEX REPLACE
+        ".*#define ENS_VERSION_NAME\ +\"([0-9a-zA-Z\ _-]+)\".*" "\\1"
+        ENSMALLEN_VERSION_NAME "${_ensmallen_HEADER_CONTENTS}")
+
+  endif()
+
+  set(ENSMALLEN_VERSION_STRING "${ENSMALLEN_VERSION_MAJOR}.${ENSMALLEN_VERSION_MINOR}.${ENSMALLEN_VERSION_PATCH}")
+endif ()
 
 # Checks 'REQUIRED', 'QUIET' and versions.
 include(FindPackageHandleStandardArgs)
